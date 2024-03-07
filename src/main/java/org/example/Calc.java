7e551681fb117c1ebcb8439a3c9d013108b98813
Vladimirpiniazhin <vladimir.piniazhin@metropolia.fi>
2024-03-07 14:48:10 +0200

    pom.xml fixed

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -4,4 +4,8 @@ public class Calc {
     public double calculation(double a, double b) {
         return a + b;
     }
+    public static void main( String[] args )    {
+        Calc calculator = new Calc();
+        calculator.calculation(2, 3);
+    }
 }
--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main
-{
-    public static void main( String[] args )    {
-        Calc calculator = new Calc();
-        calculator.calculation(2, 3);
-    }
-}
